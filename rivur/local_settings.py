1f6debc9621d407b1dab4bae30f2017750d4182e
hqpr <adubnyak@gmail.com>
2015-05-15 20:14:37 +0300

    disable local_settings

--- rivur/local_settings.py
+++ rivur/local_settings.py
@@ -1,9 +1,9 @@
-import os
-
-from settings import BASE_DIR
-from settings import STATIC_URL
-
-DEBUG = True
-
-STATIC_ROOT = os.path.join(BASE_DIR, '/static/')
-MEDIA_ROOT = '../media/'
+# import os
+#
+# from settings import BASE_DIR
+# from settings import STATIC_URL
+#
+# DEBUG = True
+#
+# STATIC_ROOT = os.path.join(BASE_DIR, '/static/')
+# MEDIA_ROOT = '../media/'
